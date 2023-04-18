44de0b5449f521d9f756636e53f9f49a23301110
Riley Knybel <spsknybeln@gmail.com>
2023-04-18 12:29:58 -0400

    Results for 0.1s granularity

--- SpectralModelingSynth/kernel.cu
+++ SpectralModelingSynth/kernel.cu
@@ -12,31 +12,27 @@
 #include <queue>
 #include <Windows.h>
 #include <mmsystem.h>
-
-std::queue<char*> audioBufferQueue; // queue to hold audio buffers
+#include <chrono>
 
 const int sampleRate = 44100;
 const int d_nInstrument = 100; // can also think of instruments as paraphonic oscillators
 const int d_nPartials = 50;
 
 //partial frequency array. Each of the 50 sine wave partials gets a frequency
-float freqs[50] = { 1000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 };
-float* h_freqs = (float*)malloc(sizeof(float) * 50);
+float h_freqs[50] = { 1000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 };
 float* d_freqs;
 
 //partial amplitude array. Each of the 50 sine wave partials gets an amplitude
-float amps[50] = { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
-float* h_amps = (float*)malloc(sizeof(float) * 50);
+float h_amps[50] = { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
 float* d_amps;
-int numSamples = 882;
 
-char* h_samples = (char*)malloc(sizeof(char) * numSamples);
-char* d_samples;
+int numSamples = 882;
+float* h_samples = (float*)malloc(sizeof(float) * numSamples);
+float* d_samples;
 
 //partial table. I want to learn what this does.
 int table_nInstruments = 50;
 int table_nPartials = 50;
-float partialEnergy[50];
 float* d_partialEnergy;
 float* h_partialArray = (float*)malloc(sizeof(float)* table_nInstruments* table_nPartials);
 float* d_partialArray;
@@ -44,54 +40,54 @@ cudaTextureObject_t texPartials;
 
 //sount fonts? samples? Arrays of normalized partial data to use for textures:
 // 
+float partialEnergy[50] = { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
 
+//// new algorithm (3?) - 
+//__global__ void new_method(
+//    float* d_freqs, // pointer to frequency array in global memory
+//    float* d_amps, // pointer to amplitude array in global memory
+//    char* d_samples, // pointer to sample array in global memory
+//    int streamNo, // the stream index
+//    float timeOffset // the time offset of current granularity
+//) {
+//    const float Pi = 3.141592;
+//    const float UnitTime = 0.01;
+//    float t = (float)(threadIdx.x) / sampleRate // sampleRate : 44100Hz
+//        + (float)streamNo * UnitTime + timeOffset; // UnitTime : 0.01s
+//    float sum = 0;
+//    float partialAmp = 1.0 / d_nPartials;
+//    // blockIdx.x: 0 for left channel, 1 for right channel.
+//    if (blockIdx.x == 0) {
+//        // block 0 computes the instrument index 0 – 49 for left channel
+//        // d_nInstrument : number of instruments. (N=100)
+//        for (int k = 0; k < d_nPartials; k++) {
+//            //float4 val = tex2D<float4>(partialTex, float(i * d_nPartials + (k - 1)), float(streamNo));
+//            float result = 1;// partialAmp;//val.x;
+//            // texPartials : the look-up table of normalized partial energy
+//            sum += d_amps[k] *
+//                result *
+//                std::sinf(2 * Pi * d_freqs[k] * t);
+//        }
+//
+//        d_samples[threadIdx.x * 2] = (char)sum;
+//    }
+//    else {
+//        // block 1 compute the instrument index 50 – 99 for right channel
+//            for (int k = 1; k <= d_nPartials; k++) {
+//                //float4 val = tex2D<float4>(partialTex, float(i * d_nPartials + (k - 1)), float(streamNo));
+//                float result = 1;// partialAmp;//val.x;
+//                sum += d_amps[k] *
+//                    result *
+//                    std::sinf(2 * Pi * d_freqs[k] * t);
+//            }
+//        d_samples[threadIdx.x * 2 + 1] = (char)sum;
+//    }
+//}
 
-// new algorithm (3?) - 
-__global__ void new_method(
-    float* d_freqs, // pointer to frequency array in global memory
-    float* d_amps, // pointer to amplitude array in global memory
-    char* d_samples, // pointer to sample array in global memory
-    int streamNo, // the stream index
-    float timeOffset // the time offset of current granularity
-) {
-    const float Pi = 3.141592;
-    const float UnitTime = 0.01;
-    float t = (float)(threadIdx.x) / sampleRate // sampleRate : 44100Hz
-        + (float)streamNo * UnitTime + timeOffset; // UnitTime : 0.01s
-    float sum = 0;
-    float partialAmp = 1.0 / d_nPartials;
-    // blockIdx.x: 0 for left channel, 1 for right channel.
-    if (blockIdx.x == 0) {
-        // block 0 computes the instrument index 0 – 49 for left channel
-        // d_nInstrument : number of instruments. (N=100)
-        for (int k = 0; k < d_nPartials; k++) {
-            //float4 val = tex2D<float4>(partialTex, float(i * d_nPartials + (k - 1)), float(streamNo));
-            float result = 1;// partialAmp;//val.x;
-            // texPartials : the look-up table of normalized partial energy
-            sum += d_amps[k] *
-                result *
-                std::sinf(2 * Pi * d_freqs[k] * t);
-        }
-
-        d_samples[threadIdx.x * 2] = (char)sum;
-    }
-    else {
-        // block 1 compute the instrument index 50 – 99 for right channel
-            for (int k = 1; k <= d_nPartials; k++) {
-                //float4 val = tex2D<float4>(partialTex, float(i * d_nPartials + (k - 1)), float(streamNo));
-                float result = 1;// partialAmp;//val.x;
-                sum += d_amps[k] *
-                    result *
-                    std::sinf(2 * Pi * d_freqs[k] * t);
-            }
-        d_samples[threadIdx.x * 2 + 1] = (char)sum;
-    }
-}
-
-__global__ void sinusoidal_mehod1(
+__global__ void sinusoidal_method1(
     float* freq, // pointer to frequency array in global memory
     float* amplitude, // pointer to amplitude array in global memory
-    char* samples, // pointer to sample array in global memory
+    float* samples, // pointer to sample array in global memory
     float* partialEnergy,
     int streamNo, // the stream index
     float timeOffset // the time offset of current granularity
@@ -115,7 +111,8 @@ __global__ void sinusoidal_mehod1(
                     std::sinf(2 * Pi * freq[i] * k * t);
             }
         }
-        samples[threadIdx.x * 2] = (char)sum;
+        samples[threadIdx.x * 2] = sum;
+        //printf("Sum: %d", sum);
     }
     else {
         // block 1 compute the instrument index 50 – 99 for right channel
@@ -126,33 +123,60 @@ __global__ void sinusoidal_mehod1(
                     std::sinf(2 * Pi * freq[i] * k * t);
             }
         }
-        samples[threadIdx.x * 2 + 1] = (char)sum;
+        samples[threadIdx.x * 2 + 1] = sum;
+        //printf("Sum: %d", sum);
     }
 }
 
-
-void CALLBACK waveOutProc(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2) {
-    if (uMsg == 955) {
-        if (!audioBufferQueue.empty()) {
-            WAVEHDR waveHdr;
-            waveHdr.lpData = audioBufferQueue.front();
-            waveHdr.dwBufferLength = 441 * sizeof(short) * 2;
-            waveHdr.dwBytesRecorded = 0;
-            waveHdr.dwUser = 0;
-            waveHdr.dwFlags = 0;
-            waveHdr.dwLoops = 0;
-            int result = waveOutPrepareHeader(hwo, &waveHdr, sizeof(WAVEHDR));
-            if (result != MMSYSERR_NOERROR) //err 12 handle being used
-            {
-                // Failed to prepare the audio buffer header
-                std::cout << result;
-                waveOutClose(hwo);
-            }
-            free(audioBufferQueue.front());
-            audioBufferQueue.pop();
-        }
-    }
-};
+//__global__ void sinusoidal_method2_1(
+//    float* freq, // pointer to frequency array in global memory
+//    float* amplitude, // pointer to amplitude array in global memory
+//    short* samples, // pointer to sample array in global memory
+//    int streamNo, // the stream index
+//    float timeOffset // the time offset of current granularity
+//) {
+//    const float Pi = 3.141592;
+//    const float UnitTime = 0.01;
+//    float t = (float)(threadIdx.x) / sampleRate // sampleRate : 44100Hz
+//        + (float)streamNo * UnitTime + timeOffset; // UnitTime : 0.01s
+//    float sum = 0;
+//    // The frequency and amplitude in one block is the same.
+//    __shared__ float f;
+//    __shared__ float a;
+//    if(threadIdx.x == 0) {
+//        f = freq[blockIdx.x];
+//        a = amplitude[blockIdx.x];
+//    }
+//    __syncthreads();
+//    // one instrument has 50 partials
+//    for (int k = 1; k <= d_nPartials; k++) {
+//        // texPartials : the look-up table of normalized partial energy
+//        sum += a *
+//            tex2D(texPartials, blockIdx.x * d_nPartials + (k - 1), streamNo)
+//            * __sinf(2 * Pi * f * k * t);
+//    }
+//    // assign sum register to temporary storing matrix in global memory
+//    stored_matrix[blockIdx.x * blockDim.x + threadIdx.x] = sum;
+//}
+///********** second kernel **********/
+//__global__ void sinusoidal_method2_2(short* samples) {
+//    float sum = 0;
+//    // blockIdx.x: 0 for left channel, 1 for right channel.
+//    if (blockIdx.x == 0) {
+//        // block 0 compute the instrument index 0 – 49 for left channel
+//        for (int i = 0; i < d_nInstrument / 2; i++)
+//            sum += stored_matrix[threadIdx.x + i * blockDim.x];
+//        // assign sum register to sample array in global memory
+//        samples[threadIdx.x * 2] = (short)sum;
+//    }
+//    else {
+//        // block 1 compute the instrument index 50 – 99 for right channel
+//        // d_nInstrument : number of instruments. (N=100)
+//        for (int i = d_nInstrument / 2; i < d_nInstrument; i++)
+//            sum += stored_matrix[threadIdx.x + i * blockDim.x];
+//        // assign sum register to sample array in global memory
+//        samples[threadIdx.x * 2 + 1] = (short)sum;
+//    }
 
 void printError() {
     cudaError_t error = cudaGetLastError();
@@ -163,160 +187,86 @@ void printError() {
 }
 
 void prepareSMS() {
-    //move array data into to host-side arrays
-    for (int i = 0; i < d_nPartials; i++) h_freqs[i] = freqs[i];
-    for (int i = 0; i < d_nPartials; i++) h_amps[i] = amps[i];
+    ////move array data into to host-side arrays
+    //for (int i = 0; i < d_nPartials; i++) h_freqs[i] = freqs[i];
+    //for (int i = 0; i < d_nPartials; i++) h_amps[i] = amps[i];
 
     //allocate GPU memory
     std::cout << "CUDA Mallocs" << std::endl;
-    cudaMalloc((void**) & d_freqs, sizeof(float) * d_nPartials);
+    cudaMalloc((void**) &d_freqs, sizeof(float) * d_nPartials);
     printError();
     cudaMalloc((void**) &d_amps, sizeof(float) * d_nPartials);
     printError();
-    cudaMalloc((void**) &d_samples, sizeof(char) * numSamples);
+    cudaMalloc((void**) &d_samples, sizeof(float) * numSamples);
     printError();
-    cudaMalloc((void**)&d_partialEnergy, sizeof(float) * table_nPartials);
+    cudaMalloc((void**) &d_partialEnergy, sizeof(float) * table_nPartials);
     printError();
 
 
     //allocate variables for the kernel
-    cudaMemcpy(d_freqs, h_freqs, sizeof(float) * d_nPartials, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_freqs, &h_freqs, sizeof(float) * d_nPartials, cudaMemcpyHostToDevice);
     std::cout << "memcopy d_freqs" << std::endl;
     printError();
-    cudaMemcpy(d_amps, h_amps, sizeof(float) * d_nPartials, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_amps, &h_amps, sizeof(float) * d_nPartials, cudaMemcpyHostToDevice);
     std::cout << "memcopy d_amps" << std::endl;
     printError();
-    cudaMemcpy(d_samples, h_samples, sizeof(char) * 882, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_samples, h_samples, sizeof(float) * numSamples, cudaMemcpyHostToDevice);
     std::cout << "memcopy d_samples" << std::endl;
     cudaMemcpy(d_partialEnergy, &partialEnergy, sizeof(float) * table_nPartials, cudaMemcpyHostToDevice);
     std::cout << "memcopy d_partialEnergy" << std::endl;
     printError();
 }
 
+
+
 void shutdownSMS() {
     //free memory
+    //TODO: fix
     cudaFree(d_freqs);
     cudaFree(d_amps);
     cudaFree(d_samples);
     cudaFree(d_partialArray);
-    free(h_freqs);
-    free(h_amps);
     free(h_samples);
     free(h_partialArray);
 }
 
-char* renderBufferNewSMS() {
-    char* new_h_samples = (char*)malloc(sizeof(char) * numSamples);
-    sinusoidal_mehod1 <<<2, 441 >>> (d_freqs, d_amps, d_samples, d_partialEnergy, 0, 0.01);
+void renderBufferNewSMS() {
+    sinusoidal_method1 <<<2, 441 >>> (d_freqs, d_amps, d_samples, d_partialEnergy, 0, 0.01);
     //copy memory from device to host
-    cudaMemcpy(new_h_samples, d_samples, numSamples * sizeof(char), cudaMemcpyDeviceToHost);
-    return new_h_samples;
+    cudaMemcpy(h_samples, d_samples, numSamples * sizeof(float), cudaMemcpyDeviceToHost);
 }
 
-// Generate and add audio buffer to the queue
-void generateAudioBufferAndAddToQueue()
-{
-    char* buffer = renderBufferNewSMS();
-    audioBufferQueue.push(buffer);
-}
-
-int playBuffer(char* buffer, int bufferSize) {
-    //audio player code
-     // Open the default waveform-audio output device
-    HWAVEOUT hWaveOut;
-    WAVEFORMATEX format;
-    format.wFormatTag = WAVE_FORMAT_PCM;
-    format.nChannels = 2;
-    format.nSamplesPerSec = 44100;
-    format.wBitsPerSample = 16;
-    format.nBlockAlign = format.nChannels * format.wBitsPerSample / 8;
-    format.nAvgBytesPerSec = format.nSamplesPerSec * format.nBlockAlign;
-    format.cbSize = 0;
-    MMRESULT result = waveOutOpen(&hWaveOut, WAVE_MAPPER, &format, (DWORD_PTR)waveOutProc, 0, CALLBACK_FUNCTION);
-    if (result != MMSYSERR_NOERROR)
-    {
-        // Failed to open the audio device
-        return 1;
-    }
-
-    // Prepare the audio buffer header
-    WAVEHDR waveHdr;
-    waveHdr.lpData = (char*)buffer;
-    waveHdr.dwBufferLength = bufferSize * sizeof(short) * format.nChannels;
-    waveHdr.dwBytesRecorded = 0;
-    waveHdr.dwUser = 0;
-    waveHdr.dwFlags = 0;
-    waveHdr.dwLoops = 0;
-    result = waveOutPrepareHeader(hWaveOut, &waveHdr, sizeof(WAVEHDR));
-    if (result != MMSYSERR_NOERROR)
-    {
-        // Failed to prepare the audio buffer header
-        waveOutClose(hWaveOut);
-        return 1;
-    }
-
-    // Play the audio buffer
-    result = waveOutWrite(hWaveOut, &waveHdr, sizeof(WAVEHDR));
-    if (result != MMSYSERR_NOERROR)
-    {
-        // Failed to start audio playback
-        waveOutUnprepareHeader(hWaveOut, &waveHdr, sizeof(WAVEHDR));
-        waveOutClose(hWaveOut);
-        return 1;
-    }
-
-    // Wait for the audio buffer to finish playing
-    while (waveHdr.dwFlags & WHDR_DONE) {
-        Sleep(1);
-    }
-
-    // Clean up
-    waveOutUnprepareHeader(hWaveOut, &waveHdr, sizeof(WAVEHDR));
-    waveOutClose(hWaveOut);
-    // delete[] audioBuffer;
-    return 0;
-}
-
-//Play audio buffers in the queue
-void playAudioBuffersInQueue()
-{
-    int buffNum = 1;
-    while (!audioBufferQueue.empty()) {
-        playBuffer(audioBufferQueue.front(), numSamples/2);
-        free(audioBufferQueue.front());
-        audioBufferQueue.pop();
-        //std::cout << "Buffer number " << buffNum << std::endl;
-        buffNum++;
+void printSamples(float* h_samples, int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << h_samples[i] << ", ";
     }
 }
-
-double MakeNoise(double dTime) {
-    return 0;
-}
-
 
 int main()
 {
-
     prepareSMS();
 
     //disregarding that 2D texture for now
     //set up partial energy texture object
 
     //all partials on full blast - the lazy way
-    for (int i = 0; i < table_nInstruments; i++) {
-            partialEnergy[i] = 1.0;
+    for (int i = 0; i < table_nPartials; i++) {
+        std::cout << partialEnergy[i] << ", ";
     }
 
     std::cout << "Generating Audio..." << std::endl;
 
+    //Time measurements
+    std::cout << "Sinusoidal Method 1 Run Times" << std::endl;
     for (int i = 0; i < 100; i++) {
-        generateAudioBufferAndAddToQueue();
+        auto t1 = std::chrono::high_resolution_clock::now();
+        renderBufferNewSMS();
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto us_int = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+        std::cout << (long long)us_int.count() << std::endl;
     }
 
-    for (int i = 0; i < numSamples; i++) {
-        std::cout << (int)h_samples[i] << ", ";
-    }
+    //printSamples(h_samples, numSamples);
 
     std::cout << std::endl;
 
