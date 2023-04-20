3e951721fddc859a393f838b0e1400ddf029211e
Riley Knybel <spsknybeln@gmail.com>
2023-04-20 04:01:40 -0400

    Testing code added, Hi CREATE!

--- SpectralModelingSynth/kernel.cu
+++ SpectralModelingSynth/kernel.cu
@@ -13,21 +13,27 @@
 #include <Windows.h>
 #include <mmsystem.h>
 #include <chrono>
+#include <fstream>
 
 const int sampleRate = 44100;
 const int d_nInstrument = 100; // can also think of instruments as paraphonic oscillators
 const int d_nPartials = 50;
 
 //partial frequency array. Each of the 50 sine wave partials gets a frequency
-float h_freqs[50] = { 1000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 };
+float h_freqs[50] = { 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 };
 float* d_freqs;
 
 //partial amplitude array. Each of the 50 sine wave partials gets an amplitude
 float h_amps[50] = { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
 float* d_amps;
 
+//GRANULATITIES
+// 0.01, 0.05, 0.1, 0.5, 1.0, 5.0, 10.0
+int granularities[] = {882, 4410, 8820, 44100, 88200, 441000, 882000};
+float granularity_times[] = { 0.01, 0.05, 0.1, 0.5, 1.0, 5.0, 10.0 };
+
 int numSamples = 882;
-float* h_samples = (float*)malloc(sizeof(float) * numSamples);
+//float* h_samples = (float*)malloc(sizeof(float) * numSamples);
 float* d_samples;
 
 //partial table. I want to learn what this does.
@@ -90,12 +96,12 @@ __global__ void sinusoidal_method1(
     float* samples, // pointer to sample array in global memory
     float* partialEnergy,
     int streamNo, // the stream index
-    float timeOffset // the time offset of current granularity
+    float* timeOffset // the time offset of current granularity
 ) {
     const float Pi = 3.141592;
     const float UnitTime = 0.01;
     float t = (float)(threadIdx.x) / sampleRate // sampleRate : 44100Hz
-        + (float)streamNo * UnitTime + timeOffset; // UnitTime : 0.01s
+        + (float)streamNo * UnitTime + *timeOffset; // UnitTime : 0.01s
     float sum = 0;
     // blockIdx.x: 0 for left channel, 1 for right channel.
     if (blockIdx.x == 0) {
@@ -186,7 +192,7 @@ void printError() {
     }
 }
 
-void prepareSMS() {
+void prepareSMS(int bufferSize) {
     ////move array data into to host-side arrays
     //for (int i = 0; i < d_nPartials; i++) h_freqs[i] = freqs[i];
     //for (int i = 0; i < d_nPartials; i++) h_amps[i] = amps[i];
@@ -197,7 +203,7 @@ void prepareSMS() {
     printError();
     cudaMalloc((void**) &d_amps, sizeof(float) * d_nPartials);
     printError();
-    cudaMalloc((void**) &d_samples, sizeof(float) * numSamples);
+    cudaMalloc((void**) &d_samples, sizeof(float) * bufferSize);
     printError();
     cudaMalloc((void**) &d_partialEnergy, sizeof(float) * table_nPartials);
     printError();
@@ -205,33 +211,34 @@ void prepareSMS() {
 
     //allocate variables for the kernel
     cudaMemcpy(d_freqs, &h_freqs, sizeof(float) * d_nPartials, cudaMemcpyHostToDevice);
-    std::cout << "memcopy d_freqs" << std::endl;
+    //std::cout << "memcopy d_freqs" << std::endl;
     printError();
     cudaMemcpy(d_amps, &h_amps, sizeof(float) * d_nPartials, cudaMemcpyHostToDevice);
-    std::cout << "memcopy d_amps" << std::endl;
+    //std::cout << "memcopy d_amps" << std::endl;
     printError();
-    cudaMemcpy(d_samples, h_samples, sizeof(float) * numSamples, cudaMemcpyHostToDevice);
-    std::cout << "memcopy d_samples" << std::endl;
+    //cudaMemcpy(d_samples, h_samples, sizeof(float) * bufferSize, cudaMemcpyHostToDevice);
+    //std::cout << "memcopy d_samples" << std::endl;
     cudaMemcpy(d_partialEnergy, &partialEnergy, sizeof(float) * table_nPartials, cudaMemcpyHostToDevice);
-    std::cout << "memcopy d_partialEnergy" << std::endl;
+    //std::cout << "memcopy d_partialEnergy" << std::endl;
     printError();
 }
 
 
 
 void shutdownSMS() {
-    //free memory
-    //TODO: fix
     cudaFree(d_freqs);
     cudaFree(d_amps);
     cudaFree(d_samples);
-    cudaFree(d_partialArray);
-    free(h_samples);
-    free(h_partialArray);
+    cudaFree(d_partialEnergy);
 }
 
-void renderBufferNewSMS() {
-    sinusoidal_method1 <<<2, 441 >>> (d_freqs, d_amps, d_samples, d_partialEnergy, 0, 0.01);
+void renderBufferNewSMS(int size, float granularity, float* h_samples) {
+    float* d_granularity;
+    cudaMalloc((void**)&d_granularity, sizeof(float));
+    cudaMemcpy(d_granularity, &granularity, sizeof(float), cudaMemcpyHostToDevice);
+    sinusoidal_method1 <<<2, 441 >>> (d_freqs, d_amps, d_samples, d_partialEnergy, 0, d_granularity);
+    printError();
+    cudaFree(d_granularity);
     //copy memory from device to host
     cudaMemcpy(h_samples, d_samples, numSamples * sizeof(float), cudaMemcpyDeviceToHost);
 }
@@ -242,9 +249,15 @@ void printSamples(float* h_samples, int size) {
     }
 }
 
+long long calcAverage(long long samps[100]) {
+    long long sum = 0;
+    for (int i = 0; i < 100; i++)
+        sum += samps[i];
+    return sum / 100;
+}
+
 int main()
 {
-    prepareSMS();
 
     //disregarding that 2D texture for now
     //set up partial energy texture object
@@ -255,15 +268,28 @@ int main()
     }
 
     std::cout << "Generating Audio..." << std::endl;
+    //MAX THREADS 1024
 
+    long long avg[100];
     //Time measurements
     std::cout << "Sinusoidal Method 1 Run Times" << std::endl;
-    for (int i = 0; i < 100; i++) {
-        auto t1 = std::chrono::high_resolution_clock::now();
-        renderBufferNewSMS();
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto us_int = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
-        std::cout << (long long)us_int.count() << std::endl;
+    for (int i = 0; i < sizeof(granularities) / sizeof(int); i++) {
+        float* h_samples = (float*)malloc(sizeof(float) * granularities[i]);
+        prepareSMS(granularities[i]);
+        std::cout << "Granularity: " << granularity_times[i] << std::endl;
+        for (int j = 0; j < 100; j++) {
+
+            auto t1 = std::chrono::high_resolution_clock::now();
+            for (int r = 0; r < granularities[i] / 882; r++) {
+                renderBufferNewSMS(granularities[i], 0.01, h_samples);
+            }
+            auto t2 = std::chrono::high_resolution_clock::now();
+            auto us_int = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+            avg[j] = (long long)us_int.count();
+        }
+        std::cout << calcAverage(avg) << std::endl;
+        shutdownSMS();
+        free(h_samples);
     }
 
     //printSamples(h_samples, numSamples);
